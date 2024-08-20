51a6b6f72e6a250a85c42eb045dfa3d00b595807
aryankumar07 <ak2839426@gmail.com>
2024-08-21 02:13:22 +0530

    s

--- app.py
+++ app.py
@@ -109,5 +109,5 @@ def disconnect():
     print(f"{name} has left the room {room}")
 
 
-if __name__=='__main__':
-    socketio.run(app,debug=True)
+# if __name__=='__main__':
+#     socketio.run(app,debug=True)
